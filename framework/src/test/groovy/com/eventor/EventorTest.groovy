52ce549dd6328b41fb8c746959667b72a02eff25
Stanislav Kurilin <st.kurilin@gmail.com>
2014-02-24 23:04:46 +0200

    Use groovy class reference feature.

--- framework/src/test/groovy/com/eventor/EventorTest.groovy
+++ framework/src/test/groovy/com/eventor/EventorTest.groovy
@@ -13,70 +13,70 @@ class EventorTest extends Specification {
     def "Event should produce aggregates"() {
         setup:
         def instanceCreator = new SimpleInstanceCreator()
-        def eventor = new Eventor([Course.class, CourseStat.class, Grades.class], instanceCreator)
-        def eb = instanceCreator.getInstanceOf(EventBus.class)
+        def eventor = new Eventor([Course, CourseStat, Grades], instanceCreator)
+        def eb = instanceCreator.getInstanceOf(EventBus)
         when:
         eb.publish(new CourseRegistered("Math-01"))
         Thread.sleep(500)
         then:
-        instanceCreator.instancies.containsKey(Course.class)
+        instanceCreator.instancies.containsKey(Course)
     }
 
     def "Event listeners should receive messages from aggregates"() {
         setup:
         def instanceCreator = new SimpleInstanceCreator()
-        def eventor = new Eventor([Course.class, CourseStat.class, Grades.class], instanceCreator)
-        def eb = instanceCreator.getInstanceOf(EventBus.class)
-        def cb = instanceCreator.getInstanceOf(CommandBus.class)
+        def eventor = new Eventor([Course, CourseStat, Grades], instanceCreator)
+        def eb = instanceCreator.getInstanceOf(EventBus)
+        def cb = instanceCreator.getInstanceOf(CommandBus)
         when:
         eb.publish(new CourseRegistered("Math-01"))
         cb.submit(new SubmitResult("Math-01", "Bob", [1, 2, 42, 1, 3] as int[]))
         cb.submit(new SubmitResult("Math-01", "Poll", [42, 2, 42, 1, 3] as int[]))
         Thread.sleep(500)
         then:
-        instanceCreator.getInstanceOf(CourseStat.class).bestResultForCourse("Math-01") == 40
+        instanceCreator.getInstanceOf(CourseStat).bestResultForCourse("Math-01") == 40
     }
 
     def "Command handlers can handle commands and produce events directly"() {
         setup:
         def instanceCreator = new SimpleInstanceCreator()
-        def eventor = new Eventor([Course.class, CourseStat.class, Grades.class], instanceCreator)
-        def eb = instanceCreator.getInstanceOf(EventBus.class)
-        def ch = instanceCreator.getInstanceOf(CourseRegistrator.class)
+        def eventor = new Eventor([Course, CourseStat, Grades], instanceCreator)
+        def eb = instanceCreator.getInstanceOf(EventBus)
+        def ch = instanceCreator.getInstanceOf(CourseRegistrator)
         ch.eventBus = eb
         when:
         ch.registerCourse("Algo-2013")
         Thread.sleep(500)
         then:
-        instanceCreator.getInstanceOf(CourseStat.class).allCourses().contains("Algo-2013")
+        instanceCreator.getInstanceOf(CourseStat).allCourses().contains("Algo-2013")
     }
 
     def "Aggregates maintain state between interactions"() {
         setup:
         def instanceCreator = new SimpleInstanceCreator()
-        def eventor = new Eventor([Course.class, CourseStat.class, Grades.class], instanceCreator)
-        def eb = instanceCreator.getInstanceOf(EventBus.class)
-        def ch = instanceCreator.getInstanceOf(CourseRegistrator.class)
+        def eventor = new Eventor([Course, CourseStat, Grades], instanceCreator)
+        def eb = instanceCreator.getInstanceOf(EventBus)
+        def ch = instanceCreator.getInstanceOf(CourseRegistrator)
         ch.eventBus = eb
-        def cb = instanceCreator.getInstanceOf(CommandBus.class)
+        def cb = instanceCreator.getInstanceOf(CommandBus)
         when:
         ch.registerCourse("Algo-2013")
         cb.submit(new SubmitResult("Algo-2013", "Bob", [42, 42, 42, 42, 42] as int[]))
         cb.submit(new SubmitResult("Algo-2013", "Poll", [42, 42, 42, 42, 42] as int[]))
         Thread.sleep(500)
         then:
-        instanceCreator.getInstanceOf(Grades.class).grade("Algo-2013", "Bob") == 120 //extra bonus for first solution
-        instanceCreator.getInstanceOf(Grades.class).grade("Algo-2013", "Poll") == 100
+        instanceCreator.getInstanceOf(Grades).grade("Algo-2013", "Bob") == 120 //extra bonus for first solution
+        instanceCreator.getInstanceOf(Grades).grade("Algo-2013", "Poll") == 100
     }
 
     def "Aggregates have independent state"() {
         setup:
         def instanceCreator = new SimpleInstanceCreator()
-        def eventor = new Eventor([Course.class, CourseStat.class, Grades.class], instanceCreator)
-        def eb = instanceCreator.getInstanceOf(EventBus.class)
-        def ch = instanceCreator.getInstanceOf(CourseRegistrator.class)
+        def eventor = new Eventor([Course, CourseStat, Grades], instanceCreator)
+        def eb = instanceCreator.getInstanceOf(EventBus)
+        def ch = instanceCreator.getInstanceOf(CourseRegistrator)
         ch.eventBus = eb
-        def cb = instanceCreator.getInstanceOf(CommandBus.class)
+        def cb = instanceCreator.getInstanceOf(CommandBus)
         when:
         ch.registerCourse("Algo-2013")
         ch.registerCourse("Math-2013")
@@ -84,7 +84,7 @@ class EventorTest extends Specification {
         cb.submit(new SubmitResult("Math-2013", "Poll", [42, 42, 42, 42, 42] as int[]))
         Thread.sleep(500)
         then:
-        instanceCreator.getInstanceOf(Grades.class).grade("Algo-2013", "Bob") == 120 //extra bonus for first solution
-        instanceCreator.getInstanceOf(Grades.class).grade("Math-2013", "Poll") == 120
+        instanceCreator.getInstanceOf(Grades).grade("Algo-2013", "Bob") == 120 //extra bonus for first solution
+        instanceCreator.getInstanceOf(Grades).grade("Math-2013", "Poll") == 120
     }
 }
